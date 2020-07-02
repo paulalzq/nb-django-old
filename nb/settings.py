30e2f39f8a23cddb1b8497b8dbfbd9d17bf1848e
Paula Alvarez Quintero <paulalzq4@pop-os.localdomain>
2020-07-02 14:52:49 -0500

    Initial commit

--- nb/settings.py
+++ nb/settings.py
@@ -147,8 +147,6 @@ PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
