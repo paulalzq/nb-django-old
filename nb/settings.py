8a212410a54712861b0a22b283a4a498b33a0d1b
Paula Alvarez Quintero <paulalzq4@pop-os.localdomain>
2020-07-02 14:37:29 -0500

    Initial commit

--- nb/settings.py
+++ nb/settings.py
@@ -143,6 +143,8 @@ CSRF_COOKIE_SECURE = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
